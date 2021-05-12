6a276ceb8a1c70b7f27bceeaf4d6b86fdd2a7cf3
Arty <artyom950@mail.ru>
2021-05-12 10:31:01 +0300

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -2,9 +2,29 @@
 * Conway's Game of Life
 */
 
+import java.awt.*;
+import javax.swing.*;
+
 public class GameOfLife {
 
+    final String NAME_OF_GAME = "Conway's Game of Life"; // window's title
+    JFrame frame; // window object
+    Canvas canvasPanel; // area to draw smth
+
     public static void main(String[] args) {
-        System.out.println("Game of Life");
+            new GameOfLife().go();
     }
+
+    void go() {
+        frame = new JFrame(NAME_OF_GAME);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close on X
+        frame.setSize(300, 300); // window size
+        frame.setLocation(200, 200); // up-left corner
+        frame.setResizable(false); // resizable or not
+        frame.setVisible(true); // visible
+
+
+
+    }
+
 }
