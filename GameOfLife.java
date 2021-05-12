dfc0925d60c929b749612d354b85a64d4a4b1d37
Arty <artyom950@mail.ru>
2021-05-12 16:29:03 +0300

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -15,11 +15,13 @@ public class GameOfLife {
     final int POINT_RADIUS = 10; // radius of dot (visual)
     final int FIELD_SIZE = (LIFE_SIZE + 1) * POINT_RADIUS - 3; // field size
     final int BTN_PANEL_HEIGHT = 58; // height of button panel (footer)
-    Random random = new Random();
     boolean[][] lifeGeneration = new boolean [LIFE_SIZE][LIFE_SIZE]; // current gen
     boolean[][] nextGeneration = new boolean [LIFE_SIZE][LIFE_SIZE]; // next gen
-    JFrame frame; // window object
+    volatile boolean goNextGeneration = false;
+    int showDelay = 200;
     Canvas canvasPanel; // area to draw smth
+    JFrame frame;
+    Random random = new Random();
 
     public static void main(String[] args) {
             new GameOfLife().go();
@@ -39,18 +41,42 @@ public class GameOfLife {
         fillButton.addActionListener(new fillButtonListener()); // button click handler
 
         JButton stepButton = new JButton("Step");
+        stepButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                processOfLife();
+                canvasPanel.repaint();
+            }
+        });
+
+        final JButton goButton = new JButton("Play");
+        goButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                goNextGeneration = !goNextGeneration;
+                goButton.setText(goNextGeneration ? "Stop" : "Play");
+            }
+        });
 
         JPanel btnPanel = new JPanel(); // button panel
         btnPanel.add(fillButton);
         btnPanel.add(stepButton);
+        btnPanel.add(goButton);
 
         frame.getContentPane().add(BorderLayout.CENTER, canvasPanel); // main field
         frame.getContentPane().add(BorderLayout.SOUTH, btnPanel); // button panel
-        frame.setVisible (true); // visible
+        frame.setVisible(true); // visible
 
+        while (true) {
+            if (goNextGeneration) {
+                processOfLife();
+                canvasPanel.repaint();
+                try {
+                    Thread.sleep(showDelay);
+                } catch (InterruptedException e) {e.printStackTrace(); }
+            }
+        }
     }
 
-    //random fill cells
+    //randomly fill cells
     public class fillButtonListener implements ActionListener {
         public void actionPerformed (ActionEvent ev) {
             //countGeneration = 1;
@@ -63,6 +89,45 @@ public class GameOfLife {
         }
     }
 
+    int countNeighbors(int x, int y) {
+        int count = 0;
+        for (int dx = -1; dx < 2; dx++) {
+            for (int dy = -1; dy < 3; dy++) {
+                int nX = x + dx;
+                int nY = y + dy;
+                nX = (nX < 0) ? LIFE_SIZE -1 : nX;
+                nY = (nY < 0) ? LIFE_SIZE -1 : nY;
+                nX = (nX > LIFE_SIZE - 1) ? 0 : nX;
+                nY = (nY > LIFE_SIZE - 1) ? 0 : nY;
+                count += (lifeGeneration[nX][nY]) ? 1 : 0;
+            }
+        }
+        if (lifeGeneration[x][y]) {
+            count--;
+        }
+        return count;
+    }
+
+
+    // main process
+    void processOfLife() {
+        for (int x = 0; x < LIFE_SIZE; x++) {
+            for (int y = 0; y < LIFE_SIZE; y++) {
+                int count = countNeighbors(x, y);
+                nextGeneration[x][y] = lifeGeneration[x][y];
+                // if are 3 neighbore around empty cells - cell become alive
+                nextGeneration[x][y] = (count == 3) ? true : nextGeneration[x][y];
+                // if cell has less than 2 or more than 3 neighbors - it will die
+                nextGeneration[x][y] = (count < 2) || (count > 3) ? false : nextGeneration[x][y];
+            }
+        }
+        for (int x = 0; x < LIFE_SIZE; x ++) {
+            System.arraycopy(nextGeneration[x], 0, lifeGeneration[x], 0, LIFE_SIZE);
+        }
+        //countGeneration++;
+    }
+
+    // visual view of filled cells
     public class Canvas extends JPanel {
         @Override
         public void paint(Graphics g) {
@@ -76,5 +141,4 @@ public class GameOfLife {
             }
         }
     }
-
 }
